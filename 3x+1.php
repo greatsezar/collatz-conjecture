3f5db0192278ec3d455c844a96a08be1c9356974
greatsezar <primeus.alperen@gmail.com>
2025-03-30 23:39:25 +0300

    3x+1.php

---
+++ 3x+1.php
@@ -0,0 +1,29 @@
+<?php
+function collatz($num) {
+    if ($num < 1) {
+        return null;
+    }
+
+    $sequence = [];
+    $maxValue = $num;
+    $iterations = 0;
+
+    while ($num != 1) {
+        $sequence[] = $num;
+        if ($num % 2 == 0) {
+            $num /= 2;
+        } else {
+            $num = 3 * $num + 1;
+        }
+        $maxValue = max($maxValue, $num);
+        $iterations++;
+    }
+    $sequence[] = 1;
+
+    return [
+        'sequence' => $sequence,
+        'max_value' => $maxValue,
+        'iterations' => $iterations
+    ];
+}
+?>
